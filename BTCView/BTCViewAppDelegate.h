946622b893a784dec8cb32b9a3caa85786320d16
Fernando Chorney <fernando@rolloutstudios.com>
2011-06-18 19:06:40 -0500

    Initial Commit

---
+++ BTCView/BTCViewAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  BTCViewAppDelegate.h
+//  BTCView
+//
+//  Created by Fernando Chorney on 11-06-18.
+//  Copyright 2011 Rollout Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTCViewAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
